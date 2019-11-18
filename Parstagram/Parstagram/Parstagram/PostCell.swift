f8d94e87da9db5e22d0414fa81ddffd742b2ce7b
narayanansriram <sriram210@yahoo.in>
2019-11-17 20:25:14 -0800

    Assignment 5 done

---
+++ Parstagram/Parstagram/Parstagram/PostCell.swift
@@ -0,0 +1,33 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Sriram Narayanan on 11/16/19.
+//  Copyright © 2019 Sriram Narayanan. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    
+    @IBOutlet var photoView: UIImageView!
+    
+    
+    @IBOutlet var usernameLabel: UILabel!
+    
+    
+    @IBOutlet var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Week5/ParseChat/ParseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParseChat
+//
+//  Created by Sriram Narayanan on 11/9/19.
+//  Copyright © 2019 Sriram Narayanan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
